1c25d80d5ec8d92bd615777673b67d632f3acc18
seokwns <seokjun0915@icloud.com>
2024-12-13 16:45:36 +0900

    feat: add pool data insert script
    - route quoter 에 풀 데이터를 추가하는 스크립트 구현

---
+++ scripts/router/insert-data.ts
@@ -0,0 +1,55 @@
+import { ethers } from "hardhat";
+import { Pool, V2Pool, V3Pool } from "./types";
+import { readFileSync } from "fs";
+
+async function main() {
+  const routeQuoterAddress = "0x00440ed78d6DE8959E0c826f1b3F0CB881F9eF97";
+  const routeQuoter = await ethers.getContractAt("RouteQuoter", routeQuoterAddress);
+
+  const dbPools: Pool[] = JSON.parse(readFileSync("dragonswap-pools.json", "utf-8")) as unknown as Pool[];
+  const klayswapPools: Pool[] = JSON.parse(readFileSync("klayswap-pools.json", "utf-8")) as unknown as Pool[];
+  const neopinPools: Pool[] = JSON.parse(readFileSync("neopin-pools.json", "utf-8")) as unknown as Pool[];
+
+  const pools = [...dbPools, ...klayswapPools, ...neopinPools].sort((a, b) => (a.liquidity > b.liquidity ? -1 : 1));
+
+  const v3Pools = pools.filter((pool) => pool.type === "v3") as V3Pool[];
+  const v2Pools = pools.filter((pool) => pool.type === "v2") as V2Pool[];
+
+  const chunkSize = 100;
+  const chunkedV3Pools = Array.from({ length: Math.ceil(v3Pools.length / chunkSize) }, (_, i) =>
+    v3Pools.slice(i * chunkSize, i * chunkSize + chunkSize),
+  );
+
+  for (const chunk of chunkedV3Pools) {
+    const tx = await routeQuoter.insertV3Pools(
+      chunk.map((pool) => pool.token0),
+      chunk.map((pool) => pool.token1),
+      chunk.map((pool) => pool.fee),
+      chunk.map((pool) => pool.pool),
+      {
+        gasLimit: 10000000,
+        gasPrice: 25000000000,
+      },
+    );
+
+    await tx.wait();
+    console.log(`Inserted ${chunk.length} V3 pools`);
+  }
+
+  // await routeQuoter.insertV2Pools(
+  //   v2Pools.map((pool) => pool.token0),
+  //   v2Pools.map((pool) => pool.token1),
+  //   v2Pools.map((pool) => pool.pool),
+  //   {
+  //     gasLimit: 10000000,
+  //     gasPrice: 25000000000,
+  //   },
+  // );
+
+  console.log("Insert data completed");
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
