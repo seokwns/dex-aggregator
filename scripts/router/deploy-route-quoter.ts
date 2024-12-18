7b3beed2fdb67114d329704a548a71fc0da5518d
seokwns <seokjun0915@icloud.com>
2024-12-18 15:30:32 +0900

    feat: add PoolLocator, SmartRouter contract deploy script
    - PoolLocator, SmartRouter 컨트랙트 배포 스크립트 추가

--- scripts/router/deploy-route-quoter.ts
+++ scripts/router/deploy-route-quoter.ts
@@ -1,10 +1,23 @@
 import { ethers } from "hardhat";
 
 async function main() {
+  const poolLocator = await ethers.deployContract("PoolLocator");
+  await poolLocator.waitForDeployment();
+
+  console.log(`PoolLocator deployed to: ${poolLocator.target}`);
+
   const routeQuoter = await ethers.deployContract("RouteQuoter");
   await routeQuoter.waitForDeployment();
 
   console.log(`RouteQuoter deployed to: ${routeQuoter.target}`);
+
+  const smartRouter = await ethers.deployContract("SmartRouter", [
+    poolLocator.target,
+    "0x19aac5f612f524b754ca7e7c41cbfa2e981a4432",
+  ]);
+  await smartRouter.waitForDeployment();
+
+  console.log(`SmartRouter deployed to: ${smartRouter.target}`);
 }
 
 main().catch((error) => {
